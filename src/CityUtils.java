88ada39affebeab077b4809f79d602759ce7da94
Danil Mazurantov <mrzdanil@gmail.com>
2021-08-05 20:29:47 +0300

    add sort

--- src/CityUtils.java
+++ src/CityUtils.java
@@ -1,8 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class CityUtils {
 
     private final static String PATH = "G:\\JProjects\\Данные\\city_ru.csv";
@@ -43,18 +46,39 @@ public class CityUtils {
      * @return {@link City}
      */
     public static City toCityFromString(String line){
-
         String[] arr = new String[5];
         StringBuilder bf = new StringBuilder(line);
         for(int i = 0; i < 5; i++){
             arr[i] = bf.substring(bf.lastIndexOf(";") + 1);
             bf.delete(bf.lastIndexOf(";"), bf.length());
-
         }
 
         return new City(arr[4], arr[3], arr[2], Integer.parseInt(arr[1]), arr[0]);
 
+    }
 
+    /**
+     * Сортировка списка городов по наименованию в алфавитном порядке
+     *
+     * @param cities список городов
+     * @return  отсортированный массив с данными о городах
+     */
+    public static List<City> sortByName(List<City> cities){
+        return cities.stream()
+                .sorted((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    /**
+     * Сортировка списка городов по федеральному округу и наименованию города внутри каждого федерального округа
+     *
+     * @param cities список городов
+     * @return отсортированный массив с данными о городах
+     */
+    public static List<City> sortByDistrictAndName(List<City> cities){
+        return cities.stream()
+                .sorted(Comparator.comparing(City::getDistrict).thenComparing(City::getName))
+                .collect(Collectors.toUnmodifiableList());
     }
 
 
